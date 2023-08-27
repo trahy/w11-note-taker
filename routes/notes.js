16c05cb17809edb954e58bef4c5fe42ee56ed569
unknown <trahy032@gmail.com>
2023-08-27 20:31:33 +0930

    added minor changes

--- routes/notes.js
+++ routes/notes.js
@@ -8,40 +8,40 @@ const {
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // GET Route for a specific note
 notes.get('/:note_id', (req, res) => {
     const noteId = req.params.note_id;
-    readFromFile('./db/notes.json')
+    readFromFile('./db/db.json')
         .then((data) => JSON.parse(data))
         .then((json) => {
             const result = json.filter((note) => note.note_id === noteId);
             return result.length > 0
-                ? res.json(result)
-                : res.json('No note with that ID');
+            ? res.json(result)
+            : res.json('No tip with that ID');
         });
-});
+    });
 
 // DELETE Route for a specific note
 notes.delete('/:note_id', (req, res) => {
     const noteId = req.params.note_id;
-    readFromFile('./db/notes.json')
+    readFromFile('./db/db.json')
         .then((data) => JSON.parse(data))
         .then((json) => {
             // Make a new array of all notes except the one with the ID provided in the URL
             const result = json.filter((note) => note.note_id !== noteId);
 
             // Save that array to the filesystem
-            writeToFile('./db/notes.json', result);
+            writeToFile('./db/db.json', result);
 
             // Respond to the DELETE request
-            res.json(`Item ${noteId} has been deleted 🗑️`);
+            res.json(`Item ${noteId} has been deleted`);
         });
 });
 
-// POST Route for a new UX/UI note
+// POST Route for a new note
 notes.post('/', (req, res) => {
     console.log(req.body);
 
@@ -54,7 +54,7 @@ notes.post('/', (req, res) => {
             note_id: uuidv4(),
         };
 
-        readAndAppend(newNote, './db/notes.json');
+        readAndAppend(newNote, './db/db.json');
         res.json(`Note added successfully`);
     } else {
         res.error('Error in adding note');
--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
+// GET Route for home page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+// GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
